0cb613e3a4a13fcdc81f10a545f336dcb2f71a71
Andrew Feng <andrewfeng@cmu.edu>
2015-07-23 00:29:47 -0700

    more home updates

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,7 +8,7 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'MainController'
 		})
 
-		.when('/jobs', {
+		.when('/jobs/:city', {
 			templateUrl: 'views/jobs.html',
 			controller: 'JobsController'
 		})
